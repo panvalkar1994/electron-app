780c81313d430b56d583b4024d10cdbf2bc17fc4
vaibhav panvalkar <vpanvalkar@outlook.com>
2021-07-08 18:31:47 +0530

    corrected

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const { app,BrowserWindow, ipcMain} = require('electron')
+const { app,BrowserWindow, ipcMain, autoUpdater, dialog} = require('electron');
 const url = require("url");
 const path = require("path");
 
@@ -50,3 +50,33 @@ app.on('activate', function () {
 ipcMain.on('app_version', (event) => {
   event.sender.send('app_version', { version: app.getVersion() });
 });
+
+const server = 'https://github.com/panvalkar1994/electron-app';
+const feed = `${server}/releases/${process.platform}/${app.getVersion()}`
+
+autoUpdater.setFeedURL({ url:feed })
+
+setInterval(() => {
+    autoUpdater.checkForUpdates()
+  }, 60000)
+
+  
+  autoUpdater.on('update-downloaded', (event, releaseNotes, releaseName) => {
+    const dialogOpts = {
+      type: 'info',
+      buttons: ['Restart', 'Later'],
+      title: 'Application Update',
+      message: process.platform === 'win32' ? releaseNotes : releaseName,
+      detail: 'A new version has been downloaded. Restart the application to apply the updates.'
+    }
+  
+    dialog.showMessageBox(dialogOpts).then((returnValue) => {
+      if (returnValue.response === 0) autoUpdater.quitAndInstall()
+    })
+  })
+
+  autoUpdater.on('error', message => {
+    console.error('There was a problem updating the application')
+    console.error(message)
+  })
+  
